e4437e29044b25379d19c89ae7a7989cbd0ef37a
Tludi <tludi00@gmail.com>
2017-04-08 20:06:58 -0700

    added week table
    Added weeks table and connected it to controller.

--- tractwork/WorkdaysViewController.swift
+++ tractwork/WorkdaysViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 import RealmSwift
 
-class WorkdaysViewController: UIViewController {
+class WorkdaysViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  let realm = try! Realm()
+  
   override func viewWillAppear(_ animated: Bool) {
     let workdays = try! Realm().objects(Workday.self)
     if let workday = workdays.last {
@@ -29,19 +31,39 @@ class WorkdaysViewController: UIViewController {
   }
   override func viewDidLoad() {
     super.viewDidLoad()
-//    let workdays = try! Realm().objects(Workday.self)
-//    if let workday = workdays.last {
-//      let timepunches = workday.timePunchPairs
-//      print("\(timepunches.count)")
-//      for punch in timepunches {
-//        print("\(punch.firstTimePunch)")
-//        print("\(punch.secondTimePunch)")
-//      }
-//    } else {
-//      print("There are no workdays")
-//    }
+
+  }
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    let days = realm.objects(Workday.self)
+    var day = Workday()
+    if days.count != 0 {
+      day = days.last!
+    }
+      print("there are \(days.count) days")
+      print("there are \(day.timePunchPairs.count) punches")
     
-    // Do any additional setup after loading the view, typically from a nib.
+    return day.timePunchPairs.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let days = realm.objects(Workday.self)
+    var day = Workday()
+//    print("there are \(days.count) days")
+    if days.count != 0 {
+      day = days.last!
+    }
+    
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "dayCell", for: indexPath)
+//    cell.textLabel?.text = "hello"
+    cell.textLabel?.text = "\(String(describing: day.timePunchPairs[indexPath.row].firstTimePunch!.punchTime))"
+    cell.detailTextLabel?.text = "\(String(describing: day.timePunchPairs[indexPath.row].secondTimePunch?.punchTime))"
+    return cell
   }
 
 
